a038de73246a688a98d965c09e141aab24d17687
Arnav Gupta <arnav@codingblocks.com>
2019-09-20 11:17:04 +0530

    js basics
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ js-basics/script.js
@@ -0,0 +1,19 @@
+let x = 10
+let y = 'asdasdasd'
+let z = true
+let arr = [2,3,5, 'asdsad', true]
+let obj = {
+  a: 10,
+  b: 'sfbsfb',
+  c: true,
+  d: [4,6,7],
+  e: {
+    p: 2456,
+    q: 246
+  }
+}
+
+function hello () {
+  console.log('Hello')
+  return 1
+}
