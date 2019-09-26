f63d033af9135bfcfd6a6d53fd2847a7ee27cf78
Arnav Gupta <arnav@codingblocks.com>
2019-09-26 12:19:42 +0530

    removing the parent element
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- dom-manipulation/todo-jquery/script.js
+++ dom-manipulation/todo-jquery/script.js
@@ -2,6 +2,20 @@
 $('#addtask').click(function() {
   $('#tasklist').append(
 
-    $('<li>').text($('#newtask').val())
+    $('<li>')
+      .append(
+        $('<button>')
+          .text('❌')
+          .click(function (ev) {
+            // ev -> the event
+            // ev.target -> the element on which click happened
+            // parent() -> the li element
+            $(ev.target).parent().remove()
+          })
+      )
+      .append($('<span>').text($('#newtask').val()))
   )
+
+  $('#newtask').val('')
+
 })
