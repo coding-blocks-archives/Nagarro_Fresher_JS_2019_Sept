54c288cf74caa76906344aa1fc31f8ad6fcc73e7
Apoorv Raj <33806110+ApoorvRaj@users.noreply.github.com>
2019-09-30 15:17:02 +0530

    Updated index.js

--- spa/index.js
+++ spa/index.js
@@ -1,67 +1,83 @@
+
 let todos = [
-  {
-    id: 1,
-    name: "Teach Class at Nagarro",
-    done: true
-  },
-  {
-    id: 2,
-    name: "Get Coffee",
-    done: false
+    {
+      id: 1,
+      name: "Teach Class at Nagarro",
+      done: true,
+      date: "2016-12-02"
+    },
+    {
+      id: 2,
+      name: "Get Coffee",
+      done: false,
+      date: "2016-11-01"
+    }
+  ];
+  
+  function render(state) {
+    return state
+      .map(todo => {
+        const classString = todo.done ? `class = "list-group-item striked"` : `class = "list-group-item"`
+        return `<li data-todo="${todo.id}" ${classString}> ${todo.name} <span style="float:right;">🗓${todo.date}</span>
+
+        </li>`;
+      })
+      .join("");
   }
-];
-
-function render(state) {
-  return state
-    .map(todo => {
-      // const li = document.createElement('li')
-      // li.classList.add("striked")
-      // document.body.append(li)
-      const classString = todo.done ? `class = "list-group-item striked"` : `class = "list-group-item"`
-      return `<li data-todo="${todo.id}" ${classString}> ${todo.name} </li>`;
+  
+  function paint() {
+    $("ul").html(render(todos));
+  }
+  const inputDate = $('#newDate')
+  function addTodo() {
+    const inputBox = $('#newTodo')
+    todos.push({
+      id: todos.length + 1,
+      name: inputBox.val(),
+      done: false,
+      date:inputDate.val()
     })
-    .join("");
-}
+    inputDate.val('')
+    inputBox.val('')
+  
+    paint()
+  }
+  
+  function sortTodos(){
+    todos.sort((d1, d2) => new Date(d1.date).getTime() - new Date(d2.date).getTime());
+  }
 
-function paint() {
-  $("ul").html(render(todos));
-}
-
-function addTodo() {
-  // document.getElementById('newTodo') != $('#newTodo')
-  const inputBox = $('#newTodo')
-  todos.push({
-    id: todos.length + 1,
-    name: inputBox.val(),
-    done: false
+  $('#sort').click(()=>{
+      sortTodos()
+      paint()
+  })
+  function removeTodos() {
+    todos = todos.filter(todo => !todo.done)
+  
+    paint()
+  }
+  
+  
+  $('ul').on("click", function (e) {
+    const idToFind = e.target.dataset.todo
+    const todo = todos.find(todo => todo.id == idToFind)
+    todo.done = !todo.done
+  
+    paint()
+  })
+  
+  $('#newTodo').on("keypress", function (e) {
+    if (e.which == 13) {
+      addTodo()
+    }
+  })
+  
+  $("#reset").click(()=>{
+    const inputBox = $('#newTodo')
+    inputDate.val('')
+    inputBox.val('')
   })
 
-  inputBox.val('')
-
-  paint()
-}
-
-
-
-function removeTodos() {
-  todos = todos.filter(todo => !todo.done)
-
-  paint()
-}
-
-
-$('ul').on("click", function (e) {
-  const idToFind = e.target.dataset.todo
-  const todo = todos.find(todo => todo.id == idToFind)
-  todo.done = !todo.done
-
-  paint()
-})
-
-$('#newTodo').on("keypress", function (e) {
-  if (e.which == 13) {
-    addTodo()
-  }
-})
-
-paint();
+  $("ul").sortable();
+  
+  paint();
