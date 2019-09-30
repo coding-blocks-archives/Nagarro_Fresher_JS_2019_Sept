f0f3ace99d9a6d37bbc1a6814e0b12de9b5caa11
Akshat Jain <akshat.jain01@nagarro.com>
2019-09-30 12:38:26 +0530

    Saved the answer into file via node

--- nodejs/modules/files.js
+++ nodejs/modules/files.js
@@ -1,8 +1,27 @@
 const fs = require('fs')
 
-const contents = fs.readFile(__dirname + '/numbers.txt', 'utf-8', function (err, result) {
-  console.log(result)
+
+
+function readFile(fileName, done) {
+  fs.readFile(__dirname + '/' + fileName, 'utf-8', done)
+}
+
+function writeFile(fileName, datatoWrite, done) {
+  fs.writeFile(__dirname + '/' + fileName, datatoWrite,  done)
+}
+
+function performComputationSum(result) {
+  let arr = result.split('\n')
+  let sum = 0;
+  arr.forEach((ele) => {
+    sum += Number(ele.replace(/[\r\n]+/gm, ""))
+  })
+  return sum
+}
+
+readFile("numbers.txt", (err, result) => {
+  let sum = performComputationSum(result)
+  writeFile("answer.txt", sum, (err, result) => {
+    console.log("Done Saved Sum:"+sum )
+  })
 })
-
-
-console.log(contents.split('\n'))
