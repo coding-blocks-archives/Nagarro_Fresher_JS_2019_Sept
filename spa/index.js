1fdb2e273f8c6ead8ac0d27e77fa19b2c008ab5e
Sanchi chawla <32547764+Sanchichawla@users.noreply.github.com>
2019-09-30 17:09:49 +0530

    updated index.js

--- spa/index.js
+++ spa/index.js
@@ -1,67 +1,99 @@
 let todos = [
-  {
-    id: 1,
-    name: "Teach Class at Nagarro",
-    done: true
-  },
-  {
-    id: 2,
-    name: "Get Coffee",
-    done: false
+    {
+      id: 1,
+      name: "Teach Class at Nagarro",
+      done: true,
+      deadline: "2019-11-09"
+    },
+    {
+      id: 2,
+      name: "Get Coffee",
+      done: false,
+      deadline: "2019-10-09"
+    }
+  ];
+  
+  function render(state) {
+    return state
+      .map(todo => {
+        const classString = todo.done ? `class = "striked"` : ''
+        return `<li data-todo="${todo.id}" ${classString}> ${todo.name} ${todo.deadline} 
+        <button data-button="up">⬆</button>
+        <button data-button="down">⬇</button> </li>`;
+      })
+      .join("");
   }
-];
 
-function render(state) {
-  return state
-    .map(todo => {
-      // const li = document.createElement('li')
-      // li.classList.add("striked")
-      // document.body.append(li)
-      const classString = todo.done ? `class = "list-group-item striked"` : `class = "list-group-item"`
-      return `<li data-todo="${todo.id}" ${classString}> ${todo.name} </li>`;
+  function reset(){
+    const inputBox = $('#newTodo')
+    const dl = $('#deadline')
+
+    inputBox.val('')
+    dl.val('')
+  }
+  
+  function paint() {
+    $("ul").html(render(todos));
+  }
+ function deleted(){
+  todos=todos.filter(todo => !todo.done)
+
+  paint();
+ } 
+  function addTodo() {
+    // document.getElementById('newTodo') != $('#newTodo')
+    const inputBox = $('#newTodo')
+    const dl = $('#deadline')
+    todos.push({
+      id: todos.length + 1,
+      name: inputBox.val(),
+      done: false,
+      deadline : dl.val()
     })
-    .join("");
-}
-
-function paint() {
-  $("ul").html(render(todos));
-}
-
-function addTodo() {
-  // document.getElementById('newTodo') != $('#newTodo')
-  const inputBox = $('#newTodo')
-  todos.push({
-    id: todos.length + 1,
-    name: inputBox.val(),
-    done: false
-  })
-
-  inputBox.val('')
-
-  paint()
-}
-
-
-
-function removeTodos() {
-  todos = todos.filter(todo => !todo.done)
-
-  paint()
-}
-
-
-$('ul').on("click", function (e) {
-  const idToFind = e.target.dataset.todo
-  const todo = todos.find(todo => todo.id == idToFind)
-  todo.done = !todo.done
-
-  paint()
-})
-
-$('#newTodo').on("keypress", function (e) {
-  if (e.which == 13) {
-    addTodo()
+   
+    inputBox.val('')
+    dl.val('')
+    
+    paint()
   }
-})
+  
+  function sortTodo(){
+    todos.sort(function(a, b) { return new Date(a.deadline) - new Date(b.deadline) })
+        paint()
+  }
+  
+  $('ul').on("click", function (e){  
+    if((e.target.dataset.button === "up" || e.target.dataset.button === "down"))
+    {
+      if(e.target.dataset.button === "up")
+      {
+        if(e.target.parentElement.previousElementSibling == null)
+          return;
+        e.target.parentElement.parentElement.insertBefore(
+          e.target.parentElement,
+          e.target.parentElement.previousElementSibling
+        )
+      }
+      else
+      {
+        if(e.target.parentElement.nextElementSibling == null)
+          return;
+        e.target.parentElement.parentElement.insertBefore(
+          e.target.parentElement.nextElementSibling,
+          e.target.parentElement
+        )
+      }
+    }
 
-paint();
+    else
+    {
+        
+      const idToFind = e.target.dataset.todo
+      const todo = todos.find(todo => todo.id == idToFind)
+      todo.done = !todo.done
+      paint();
+  
+    }
+  })
+ 
+  paint();
