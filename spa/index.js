0c98a19d583a97ad88a1143d97ad702ebdca58ba
Ankit Sardana <33087507+ankitsardana@users.noreply.github.com>
2019-09-30 15:31:57 +0530

    updated index.js

--- spa/index.js
+++ spa/index.js
@@ -1,67 +1,96 @@
 let todos = [
-  {
-    id: 1,
-    name: "Teach Class at Nagarro",
-    done: true
-  },
-  {
-    id: 2,
-    name: "Get Coffee",
-    done: false
+  ];
+  $('ul').sortable();
+  
+  function render(state) {
+    
+    
+    return state
+      .map(todo => {
+        const classString = todo.done ? `class = "list-group-item striked"` : `class = "list-group-item"`
+        return `<li data-todo="${todo.id}" ${classString}  style="background-color: lightblue;"> <b>${todo.name}</b>    
+        <span style="float: right ;">🗓<b>${todo.deadline}</b></span>
+        </li>
+        `  ;
+      })
+      .join("");
   }
-];
-
-function render(state) {
-  return state
-    .map(todo => {
-      // const li = document.createElement('li')
-      // li.classList.add("striked")
-      // document.body.append(li)
-      const classString = todo.done ? `class = "list-group-item striked"` : `class = "list-group-item"`
-      return `<li data-todo="${todo.id}" ${classString}> ${todo.name} </li>`;
+  
+  function paint() {
+    $("ul").html(render(todos));
+  }
+  
+  
+  
+  function addTodo() {
+    const newDateBox=$('#newTodoDate')
+    const inputBox = $('#newTodo')
+    if(inputBox.val() == '' || newDateBox.val() == '')
+    return;
+    
+    todos.push({
+      id: todos.length + 1,
+      name: inputBox.val(),
+      done: false,
+      deadline:newDateBox.val()
     })
-    .join("");
-}
-
-function paint() {
-  $("ul").html(render(todos));
-}
-
-function addTodo() {
-  // document.getElementById('newTodo') != $('#newTodo')
-  const inputBox = $('#newTodo')
-  todos.push({
-    id: todos.length + 1,
-    name: inputBox.val(),
-    done: false
-  })
-
-  inputBox.val('')
-
-  paint()
-}
-
-
-
-function removeTodos() {
-  todos = todos.filter(todo => !todo.done)
-
-  paint()
-}
-
-
-$('ul').on("click", function (e) {
-  const idToFind = e.target.dataset.todo
-  const todo = todos.find(todo => todo.id == idToFind)
-  todo.done = !todo.done
-
-  paint()
-})
-
-$('#newTodo').on("keypress", function (e) {
-  if (e.which == 13) {
-    addTodo()
+  
+    inputBox.val('')
+    newDateBox.val('')
+  
+    paint()
   }
-})
+  
+  
+  
+  function removeTodos() {
+    todos = todos.filter(todo => !todo.done)
+  
+    paint()
+  }
+  function sortTodos(){
+    todos.sort((a, b)=> {
+        var dateA = new Date(a.deadline);
+        var dateB = new Date(b.deadline);
+        return dateA - dateB;
+      })
+  }
+ 
+  
+  
+  $('ul').on("click", function (e) {
+    const idToFind = e.target.dataset.todo
+    
+    const todo = todos.find(todo => todo.id == idToFind)
+    
+    if (todo!==undefined) {
+    todo.done = !todo.done
+    }
+    paint()
+  });
+  
+  
+  $('#newTodo').on("keypress", function (e) {
+    if (e.which == 13) {
+      addTodo()
+    }
+  })
+  $('#newTodoDate').on("keypress", function (e) {
+    if (e.which == 13) {
+      addTodo()
+    }
+  })
+  
+  
+  $('#sort').click(()=>{
+    sortTodos();
+    paint();
+});
 
-paint();
+  
+  $('#reset').click(()=>{
+    $('#newTodo').val('')
+    $('#newTodoDate').val('')
+  })
+  
+  paint();
