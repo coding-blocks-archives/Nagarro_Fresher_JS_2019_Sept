9dfeac01cf020a0aa1c497fbf22afedbcb6cf86a
Ajay Goswami <42946028+ajaygoswami0007@users.noreply.github.com>
2019-10-01 17:17:38 +0530

    Update index.js

--- spa/index.js
+++ spa/index.js
@@ -1,54 +1,90 @@
 let todos = [
   {
-    id: 1,
-    name: "Teach Class at Nagarro",
-    done: true
+      id: 1,
+      name: 'Teach class at Nagarro',
+      done: true,
+      deadline: "2019-09-20"
   },
   {
-    id: 2,
-    name: "Get Coffee",
-    done: false
+      id: 2,
+      name: 'Get Coffee',
+      done: false,
+      deadline: "2019-09-10"
   }
+
 ];
 
 function render(state) {
   return state
-    .map(todo => {
-      // const li = document.createElement('li')
-      // li.classList.add("striked")
-      // document.body.append(li)
-      const classString = todo.done ? `class = "list-group-item striked"` : `class = "list-group-item"`
-      return `<li data-todo="${todo.id}" ${classString}> ${todo.name} </li>`;
-    })
-    .join("");
+      .map(todo => {
+          const classString = todo.done ? `list-group-item striked` : `list-group-item`
+          return `<li data-todo="${todo.id}" class = "draggable ${classString}" draggable="true"> ${todo.name} <span style="float: right"> ${todo.deadline} </span></li>`;
+          
+      })
+
+      .join("");
 }
+
 
 function paint() {
   $("ul").html(render(todos));
 }
 
+
 function addTodo() {
-  // document.getElementById('newTodo') != $('#newTodo')
   const inputBox = $('#newTodo')
+  const inputDate = $('#newDate')
   todos.push({
-    id: todos.length + 1,
-    name: inputBox.val(),
-    done: false
+      id: todos.length + 1,
+      name: inputBox.val(),
+      done: false,
+      deadline: inputDate.val()
   })
 
   inputBox.val('')
+  inputDate.val('')
 
   paint()
 }
-
-
 
 function removeTodos() {
   todos = todos.filter(todo => !todo.done)
 
+  paint();
+}
+
+
+
+function sortTodos() {
+
+  todos.sort(function (a, b) {
+      return (new Date(a.deadline)) - (new Date(b.deadline))
+  });
   paint()
 }
 
+
+
+$("#sortable").sortable({
+  update: function (event, ui) {
+      let temp = [];
+      var idsInOrder = $("#sortable").sortable().toArray();
+
+      children = idsInOrder[0].children;
+      for (var i = 0; i < children.length; i++) {
+          temp[i] = todos.find(todo => todo.id == children[i].attributes['0'].value)
+      }
+      todos = temp;
+  }
+});
+
+
+function resetBox() {
+  const inputBox = $('#newTodo')
+  const inputDate = $('#newDate')
+  inputBox.val('')
+  inputDate.val('')
+}
 
 $('ul').on("click", function (e) {
   const idToFind = e.target.dataset.todo
@@ -58,10 +94,12 @@ $('ul').on("click", function (e) {
   paint()
 })
 
+
 $('#newTodo').on("keypress", function (e) {
   if (e.which == 13) {
-    addTodo()
+      addTodo();
   }
 })
+
 
 paint();
